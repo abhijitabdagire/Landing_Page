eff2b5929f6e830920673305420ed6d8d5c5a16f
abhijitabdagire <125788349+abhijitabdagire@users.noreply.github.com>
2023-02-19 18:02:17 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,108 @@
+// first question div
+
+let content_display = document.getElementById('Plus-icon');
+let content_close = document.getElementById('cross-icon');
+
+content_display.addEventListener ('click', () => {
+    document.getElementById('show_1').style.display = "block";
+    document.getElementById('cross-icon').style.display = "block";
+    document.getElementById('Plus-icon').style.display = "none";
+}
+);
+content_close.addEventListener ('click', () => {
+    document.getElementById('show_1').style.display = "none";
+    document.getElementById('cross-icon').style.display = "none";
+    document.getElementById('Plus-icon').style.display = "block";
+}
+);
+
+// second question div
+
+let content_display_1 = document.getElementById('Plus-icon-1');
+let content_close_1 = document.getElementById('cross-icon-1');
+
+content_display_1.addEventListener ('click', () => {
+    document.getElementById('show_2').style.display = "block";
+    document.getElementById('cross-icon-1').style.display = "block";
+    document.getElementById('Plus-icon-1').style.display = "none";
+}
+);
+content_close_1.addEventListener ('click', () => {
+    document.getElementById('show_2').style.display = "none";
+    document.getElementById('cross-icon-1').style.display = "none";
+    document.getElementById('Plus-icon-1').style.display = "block";
+}
+);
+
+// Third question div
+
+let content_display_2 = document.getElementById('Plus-icon-2');
+let content_close_2 = document.getElementById('cross-icon-2');
+
+content_display_2.addEventListener ('click', () => {
+    document.getElementById('show_3').style.display = "block";
+    document.getElementById('cross-icon-2').style.display = "block";
+    document.getElementById('Plus-icon-2').style.display = "none";
+}
+);
+content_close_2.addEventListener ('click', () => {
+    document.getElementById('show_3').style.display = "none";
+    document.getElementById('cross-icon-2').style.display = "none";
+    document.getElementById('Plus-icon-2').style.display = "block";
+}
+);
+
+// Fourth question div
+
+let content_display_3 = document.getElementById('Plus-icon-3');
+let content_close_3 = document.getElementById('cross-icon-3');
+
+content_display_3.addEventListener ('click', () => {
+    document.getElementById('show_4').style.display = "block";
+    document.getElementById('cross-icon-3').style.display = "block";
+    document.getElementById('Plus-icon-3').style.display = "none";
+}
+);
+content_close_3.addEventListener ('click', () => {
+    document.getElementById('show_4').style.display = "none";
+    document.getElementById('cross-icon-3').style.display = "none";
+    document.getElementById('Plus-icon-3').style.display = "block";
+}
+);
+
+// Fith question div
+
+let content_display_4 = document.getElementById('Plus-icon-4');
+let content_close_4 = document.getElementById('cross-icon-4');
+
+content_display_4.addEventListener ('click', () => {
+    document.getElementById('show_5').style.display = "block";
+    document.getElementById('cross-icon-4').style.display = "block";
+    document.getElementById('Plus-icon-4').style.display = "none";
+}
+);
+content_close_4.addEventListener ('click', () => {
+    document.getElementById('show_5').style.display = "none";
+    document.getElementById('cross-icon-4').style.display = "none";
+    document.getElementById('Plus-icon-4').style.display = "block";
+}
+);
+// Sixth question div
+
+let content_display_5 = document.getElementById('Plus-icon-5');
+let content_close_5 = document.getElementById('cross-icon-5');
+
+content_display_5.addEventListener ('click', () => {
+    document.getElementById('show_6').style.display = "block";
+    document.getElementById('cross-icon-5').style.display = "block";
+    document.getElementById('Plus-icon-5').style.display = "none";
+}
+);
+content_close_5.addEventListener ('click', () => {
+    document.getElementById('show_6').style.display = "none";
+    document.getElementById('cross-icon-5').style.display = "none";
+    document.getElementById('Plus-icon-5').style.display = "block";
+}
+);
+
+
